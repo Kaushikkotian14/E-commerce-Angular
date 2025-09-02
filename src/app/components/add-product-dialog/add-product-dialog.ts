0251f0d129433a97a7d724de59ed2303706f41f6
Kaushik <kaushikkotian003@gmail.com>
2025-09-02 23:18:39 +0530

    02-09-2025

--- src/app/components/add-product-dialog/add-product-dialog.ts
+++ src/app/components/add-product-dialog/add-product-dialog.ts
@@ -1,22 +1,22 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
-import { FormBuilder, FormGroup,FormArray, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, ReactiveFormsModule, Validators } from '@angular/forms';
 import { productModel } from '../../core/models/product.model';
 import { CategoryModel } from '../../core/models/category.model';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFileUploadModule } from 'mat-file-upload';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-add-product-dialog',
-  imports: [ReactiveFormsModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule,MatFileUploadModule,MatSelectModule],
+  imports: [ReactiveFormsModule,MatDialogModule,MatFormFieldModule,MatInputModule,MatButtonModule,MatSelectModule,MatIconModule],
   templateUrl: './add-product-dialog.html',
-  styleUrl: './add-product-dialog.scss'
+  styleUrls: ['./add-product-dialog.scss'],
 })
 export class AddProductDialog {
-public productForm: FormGroup;
+  public productForm: FormGroup;
 
   constructor(
     private fb: FormBuilder,
@@ -25,7 +25,11 @@ public productForm: FormGroup;
   ) {
     this.productForm = this.fb.group({
       productName: [data?.productName || '', Validators.required],
-      img: [data?.img || '', Validators.required],
+      img: this.fb.array(
+        data?.img?.length
+          ? data.img.map((url) => this.fb.control(url, Validators.required))
+          : [this.fb.control('', Validators.required)]
+      ),
       description: [data?.description || '', Validators.required],
       category: [data?.category || '', Validators.required],
       cost: [data?.cost || null, Validators.required],
@@ -33,28 +37,38 @@ public productForm: FormGroup;
     });
   }
 
-   catogeries: CategoryModel[] = [
-    {name: 'Electronic Appliances', value: 'Electronic Appliances'},
-    {name: 'Grocery', value: 'Grocery'},
-    {name: 'Cosmatics', value: 'Cosmatics'},
-    {name: 'Fashion', value: 'Fashion'},
-    {name: 'Food', value: 'Food'},
-    {name: 'Toys', value: 'Toys'},
-    {name: 'Sports', value: 'Sports'},
-  ];
+  get imgArray(): FormArray {
+    return this.productForm.get('img') as FormArray;
+  }
+
+  public addImageField() {
+    this.imgArray.push(this.fb.control('', Validators.required));
+  }
+
+  public removeImageField(index: number) {
+    this.imgArray.removeAt(index);
+  }
 
   public save() {
     if (this.productForm.valid) {
-     
       this.dialogRef.close({
-        ...this.data, ...this.productForm.value
+        ...this.data,
+        ...this.productForm.value,
       });
     }
-
   }
 
   public close() {
     this.dialogRef.close();
   }
-  
+
+  categories: CategoryModel[] = [
+    { name: 'Electronic Appliances', value: 'Electronic Appliances' },
+    { name: 'Grocery', value: 'Grocery' },
+    { name: 'Cosmetics', value: 'Cosmetics' },
+    { name: 'Fashion', value: 'Fashion' },
+    { name: 'Food', value: 'Food' },
+    { name: 'Toys', value: 'Toys' },
+    { name: 'Sports', value: 'Sports' },
+  ];
 }
